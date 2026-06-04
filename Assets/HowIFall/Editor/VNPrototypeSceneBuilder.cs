564f812abf0dede2605493137ff6e73797aa667c
Badgrif <aswd@asd.com>
2026-06-04 17:34:57 +0300

    Увеличен персонаж в VN демо-сцене

--- Assets/HowIFall/Editor/VNPrototypeSceneBuilder.cs
+++ Assets/HowIFall/Editor/VNPrototypeSceneBuilder.cs
@@ -103,11 +103,11 @@ public static class VNPrototypeSceneBuilder
         controller.dialogueText = dialogueText;
         controller.backgroundImage = backgroundImage;
         controller.characterImage = characterImage;
-        controller.characterLeftPosition = new Vector2(-220f, -110f);
-        controller.characterCenterPosition = new Vector2(-60f, -110f);
-        controller.characterRightPosition = new Vector2(260f, -110f);
-        controller.characterSoloPosition = new Vector2(-100f, -110f);
-        controller.characterDefaultSize = new Vector2(700f, 980f);
+        controller.characterLeftPosition = new Vector2(-130f, -360f);
+        controller.characterCenterPosition = new Vector2(0f, -360f);
+        controller.characterRightPosition = new Vector2(260f, -360f);
+        controller.characterSoloPosition = new Vector2(-40f, -360f);
+        controller.characterDefaultSize = new Vector2(980f, 1380f);
         controller.nameBox = nameBox;
         controller.nextButton = nextButton;
         controller.choicePanel = choicePanel;
@@ -202,8 +202,8 @@ public static class VNPrototypeSceneBuilder
         rect.anchorMin = new Vector2(0.5f, 0f);
         rect.anchorMax = new Vector2(0.5f, 0f);
         rect.pivot = new Vector2(0.5f, 0f);
-        rect.anchoredPosition = new Vector2(-220f, -110f);
-        rect.sizeDelta = new Vector2(700f, 980f);
+        rect.anchoredPosition = new Vector2(-130f, -360f);
+        rect.sizeDelta = new Vector2(980f, 1380f);
 
         Image image = character.AddComponent<Image>();
         image.color = Color.white;
